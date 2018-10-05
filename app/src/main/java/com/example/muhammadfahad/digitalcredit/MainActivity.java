f01a0777cc11a219b3c2abf8c2e585e379b19cd1
Github <mfahad88@outlook.com>
2018-10-05 17:13:24 +0500

    05:13PM

--- app/src/main/java/com/example/muhammadfahad/digitalcredit/MainActivity.java
+++ app/src/main/java/com/example/muhammadfahad/digitalcredit/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.muhammadfahad.digitalcredit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        btn.setOnClickListener(this);
+
+    }
+
+    private void init(){
+        btn=findViewById(R.id.button);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.button){
+            Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
     }
 }
