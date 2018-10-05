4741cbdf5ad37f9d14cbf30412e98e54ed314544
Github <mfahad88@outlook.com>
2018-10-05 18:13:13 +0500

    06:05PM

---
+++ app/src/main/java/com/example/muhammadfahad/digitalcredit/AvailLoanFragment.java
@@ -0,0 +1,30 @@
+package com.example.muhammadfahad.digitalcredit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AvailLoanFragment extends Fragment {
+
+
+    public AvailLoanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_avail_loan, container, false);
+        return view;
+    }
+
+}
