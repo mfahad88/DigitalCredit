a62b4a52709ebe1e4ea7cb51a93e499c49b6e5b4
fahad <mfahad88@outlook.com>
2018-10-07 12:49:18 +0500

    07-10-2018

--- app/src/main/java/com/example/muhammadfahad/digitalcredit/DashboardFragment.java
+++ app/src/main/java/com/example/muhammadfahad/digitalcredit/DashboardFragment.java
@@ -3,35 +3,32 @@ package com.example.muhammadfahad.digitalcredit;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AvailLoanFragment extends Fragment {
-    public static AvailLoanFragment fragment;
+public class DashboardFragment extends Fragment {
+
+    public static DashboardFragment fragment;
 
 
-    public static AvailLoanFragment getInstance() {
+    public static DashboardFragment getInstance() {
         if(fragment==null){
-            Log.e("AvailLoan","Object Created...");
-            fragment=new AvailLoanFragment();
+            fragment=new DashboardFragment();
         }
         return fragment;
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_avail_loan, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
 }
--- app/src/main/java/com/example/muhammadfahad/digitalcredit/HomeActivity.java
+++ app/src/main/java/com/example/muhammadfahad/digitalcredit/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.example.muhammadfahad.digitalcredit;
 
+import android.app.FragmentManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,10 +16,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    FrameLayout layout;
+    FragmentTransaction ft;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +32,7 @@ public class HomeActivity extends AppCompatActivity
         toolbar.setTitle("Dashboard");
 //        toolbar.setTitleTextColor(Color.parseColor("#000000"));
         setSupportActionBar(toolbar);
-
-
+        layout=findViewById(R.id.view_container);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -35,6 +41,10 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        fragment=DashboardFragment.getInstance();
+        ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.view_container,fragment);
+        ft.commit();
     }
 
     @Override
@@ -52,7 +62,9 @@ public class HomeActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        if(id==R.id.avail_loan){
+            fragment=AvailLoanFragment.getInstance();
+        }
        /* if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
@@ -66,7 +78,9 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }*/
-
+        ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.view_container,fragment);
+        ft.commit();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
